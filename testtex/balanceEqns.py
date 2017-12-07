b2868d2620350e1542bdd21813798f1e33fade3c
Anthony Salerno <anthony.saleno@sickkids.ca>
2017-12-06 20:13:31 -0500

    I think the redox equation solver is working. Needs testing

--- testtex/balanceEqns.py
+++ testtex/balanceEqns.py
@@ -5,6 +5,7 @@ from sympy import symbols
 import argparse
 from sys import argv
 import periodictable as pt
+from copy import copy
 
 '''
 This program exists to try and balance all types of equations. First it will handle simple stoich, then 
@@ -41,6 +42,17 @@ ostates[53] = -1
 ostates0 = np.where(ostates==0)[0]
 
 
+def lcf(vals):
+    vals=abs(vals)
+    v1=min(vals)
+    v2=max(vals)
+    for i in range(v1,v1*v2+1):
+        if v1%i==0 and v2%i==0:
+            return i
+def apply_lcf(vals):
+    l = lcf(vals)
+    return np.array([abs(val)/l for val in vals])
+
 def chemical_equation_string(vals,atype):
     r = vals[0]
     p = vals[1]
@@ -57,29 +69,53 @@ def chemical_equation_string(vals,atype):
     return ' '.join(s)
 
 
-def redox_balancer(rd,pd,e,ifbasic=False):
+def redox_balancer(rd,pd,isbasic=False):
+    e=rd[1]-pd[1]
     r1=chempy.Substance.from_formula(rd[0])
     p1=chempy.Substance.from_formula(pd[0])
+    reactants={}
+    products={}
+    reactants[rd[0]]=1
+    products[pd[0]]=1
     overallCharge = r1.charge-p1.charge-e
     if e > 0: # Add electrons to reactants
-        reactants=[rd[0],str(abs(e))+'e-']
-        products=[pd[0]]
-            
+        reactants['e-']=abs(e)
     if e < 0: # Add electrons to products
-        reactants=[rd[0]]
-        products=[pd[0],str(abs(e))+'e-']
-
+        products['e-']=abs(e)
     if overallCharge < 0:
-        reactants.append(str(abs(overallCharge))+'H+')
+        reactants['H+']= abs(overallCharge)
     elif overallCharge > 0:
-        products.append(str(abs(overallCharge))+'H+')
+        products['H+']= abs(overallCharge)
    
     if r1.composition[8] > p1.composition[8]:
-        products.append(str(r1.composition[8] - p1.composition[8])+'H2O')
+        products['H2O'] = r1.composition[8] - p1.composition[8]
     elif r1.composition[8] < p1.composition[8]:
-        reactants.append(str(-r1.composition[8] + p1.composition[8])+'H2O')
-    
-    return reactants, products
+        reactants['H2O'] = p1.composition[8] - r1.composition[8]
+
+    if isbasic:
+        if 'H+' in reactants.keys():
+            if 'H2O' in reactants.keys():
+                reactants['H2O']+=reactants['H+']
+            else:
+                reactants['H2O']=reactants['H+']
+            products['OH-']=reactants.pop('H+',0)
+
+        elif 'H+' in products.keys():
+            if 'H2O' in products.keys():
+                products['H2O']+=products['H+']
+            else:
+                products['H2O']=products['H+']
+            reactants['OH-']=products.pop('H+',0)
+
+        if 'H2O' in reactants.keys() and 'H2O' in products.keys():
+            numH2O = reactants['H2O'] - products['H2O']
+            if numH2O > 0:
+                products.pop('H2O')
+                reactants['H2O']=abs(numH2O)
+            elif numH2O < 0:
+                reactants.pop('H2O')
+                products['H2O']=abs(numH2O)
+    return [reactants, products]
         
 
 
@@ -107,9 +143,7 @@ def chemical_equation_latex(vals,atype):
 def get_oxidation_state(r,p):
     rd = {}
     pd = {}
-    echange = {}
     rxns = []
-    water=['H2O','O2','H+']
     for chem in r:
         chem=chempy.Substance.from_formula(chem)
         # oxidation state!
@@ -128,14 +162,8 @@ def get_oxidation_state(r,p):
     pk = pd.keys()
     elems = [val for val in rk if val in pk]
     for el in elems:
-        echange[el]=rd[el]-pd[el]
-        if echange[el] > 0:
-            rr['e-'] = echange[el]
-        if echange[el] < 0:
-            pr['e-'] = abs(echange[el])
-                
-    
-    return rr, pr, echange
+        rxns.append(redox_balancer(rd[el],pd[el]))        
+    return rxns
     
     
     
@@ -161,9 +189,25 @@ else:
 r,p = [s.split('+') for s in eqn.split(atype)]
 
 if redox:
-    rr, pr, echange = get_oxidation_state(r,p)
+    rxns = get_oxidation_state(r,p)
+    halfrxns = copy(rxns)
     K1, K2, Kw = symbols('K1 K2 Kw')
-    r.append(
+    e1=chempy.Equilibrium(rxns[0][0],rxns[0][1],K1)
+    e2=chempy.Equilibrium(rxns[1][0],rxns[1][1],K2)
+    coeffs=chempy.Equilibrium.eliminate([e1,e2],'e-')
+    coeffs=apply_lcf(np.array(coeffs,int))
+    # now we apply the coefficients and pop out the e-
+    halfrxnsStr=[]
+    rxnsStr=[]
+    for i in range(len(rxns)):
+        # First ensure that we write out the half rxns, then the full
+        for j in range(len(rxns[i])):
+            for k in rxns[i][j].keys():
+                halfrxns[i][j].k *= coeffs[i]
+                rxns[i][j].k *= coeffs[i]
+        
+        rxnsStr.append(rxns[i])
+
 else:
     bal = chempy.balance_stoichiometry(r,p)
 
